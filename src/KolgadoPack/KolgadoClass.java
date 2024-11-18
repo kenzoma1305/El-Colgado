32c54482add60930a26ff47d2081596523f26545
kenzoma1305 <makenzopowers@gmail.com>
2024-11-18 19:30:55 +0100

    Creación de las primeras variables

--- src/KolgadoPack/KolgadoClass.java
+++ src/KolgadoPack/KolgadoClass.java
@@ -1,10 +1,24 @@
 package KolgadoPack;
-
+import java.util.Scanner;
 public class KolgadoClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		String palabraOculta = "baldufa";
+		int LetrasPalabraOculta = palabraOculta.length();
+		
+	
+		char [] palabraGuiones = new char[LetrasPalabraOculta];
+		for (int i = 0; i < palabraGuiones.length; i++) {
+			palabraGuiones[i] = '_';
+		}
+		
+		int intentos = 6;
+		int letrasAdivinadas = 0;
+		
+		
+		
 	}
 
 }

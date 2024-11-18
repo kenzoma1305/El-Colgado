ea31f2b5f2b74639c9efc9ac8f81867207f7ce77
kenzoma1305 <makenzopowers@gmail.com>
2024-11-18 19:32:31 +0100

    Comentarios de los elementos iniciales.

--- src/KolgadoPack/KolgadoClass.java
+++ src/KolgadoPack/KolgadoClass.java
@@ -5,15 +5,17 @@ public class KolgadoClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		//Creamos la palabra oculta para el juego con un string
 		String palabraOculta = "baldufa";
 		int LetrasPalabraOculta = palabraOculta.length();
 		
-	
+		//Comenzamos el juego con las palabras ocultas con un guión bajo
 		char [] palabraGuiones = new char[LetrasPalabraOculta];
 		for (int i = 0; i < palabraGuiones.length; i++) {
 			palabraGuiones[i] = '_';
 		}
 		
+		//Creamos variables para almacenar los intentos y las letras adivinadas (como contador)
 		int intentos = 6;
 		int letrasAdivinadas = 0;
 		

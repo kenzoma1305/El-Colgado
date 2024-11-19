e1fcea9a39b7f72cdfaf4e690de962c0e657d8e0
kenzoma1305 <makenzopowers@gmail.com>
2024-11-19 16:21:40 +0100

    Creación del bucle principal.
    He creado el bucle principal while y algunas anotaciones.

--- src/KolgadoPack/KolgadoClass.java
+++ src/KolgadoPack/KolgadoClass.java
@@ -5,6 +5,8 @@ public class KolgadoClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		Scanner usuario = new Scanner(System.in);
+		
 		//Creamos la palabra oculta para el juego con un string
 		String palabraOculta = "baldufa";
 		int LetrasPalabraOculta = palabraOculta.length();
@@ -19,6 +21,31 @@ public class KolgadoClass {
 		int intentos = 6;
 		int letrasAdivinadas = 0;
 		
+		//Imprimimos dos mensajes para introducir el juego
+		System.out.println("¡Debemos salvar a nuestro amigo Pepito!");
+		System.out.println("¿Podrás adivinar la palabra oculta?");
+		
+		//Creamos un bucle mientras queden intentos y no se haya adivinado toda la palabra.
+		while (intentos > 0 && letrasAdivinadas < LetrasPalabraOculta) {
+			//Nos muestra el progreso de la partida
+			System.out.println("Palabra: ");
+			for (char c : palabraGuiones) {
+				System.out.println(c + "");
+			}
+			System.out.println("\nIntentos restantes: " + intentos);
+			
+			System.out.println("Escribe una letra: ");
+			char letra = usuario.nextLine().toLowerCase().charAt(0);
+			
+			int aciertos = 0;
+			for (int i = 0; i < palabraOculta.length(); i++) {
+				if (palabraOculta.charAt(i) == letra && palabraGuiones[i] == '_') {
+					palabraGuiones[i] = letra;
+					aciertos++;
+					letrasAdivinadas++;
+				}
+			}
+		}
 		
 		
 	}
